7221586fca2bc3dbf3341a4fcd9645f4a550a63e
Nathan <s3717205@student.rmit.edu.au>
2021-08-02 16:06:00 +1000

    Implemented initial alarm function

--- AlarmClock.py
+++ AlarmClock.py
@@ -1,7 +1,32 @@
+from datetime import datetime
 import time
 
-currentTime = time.localtime()
-timeString = time.strftime("%m/%d/%Y, %H:%M:%S", currentTime)
+# Set hour and minute for alarm
+print("Enter hour: ", end="")
+input_hour = int(input())
 
-print("Current Time: " + timeString)
-    
+print("Enter minute: ", end="")
+input_minute = int(input())
+
+# Print the current time
+now = datetime.now()
+currentTime = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
+print(currentTime)
+
+# creates new datetime from entered hour and minute
+input_time = datetime(now.year, now.month, now.day, input_hour, input_minute)
+
+initialSleep = True
+
+while(input_time.hour != now.hour or input_time.minute != now.minute):
+    time.sleep(60 - now.second)
+        
+    now = datetime.now()
+    currentTime = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
+    print(currentTime)
+
+
+print("!!!ALARM GOING OFF!!!")
+
+
+
